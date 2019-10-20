bc62d1e9c2f39bddfe48945737bfb0e1f5cb7fc8
AndriiSameliuk <andrii.sameliuk@teamdev.com>
2019-10-20 17:06:10 +0300

    Add `DaoFactory`

---
+++ src/main/java/ua/nure/cs/sameliuk/usermanagment/db/DaoFactory.java
@@ -0,0 +1,19 @@
+package ua.nure.cs.sameliuk.usermanagment.db;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class DaoFactory {
+
+    private final Properties properties;
+
+    public DaoFactory() {
+        this.properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader()
+                                      .getResourceAsStream("settings.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
