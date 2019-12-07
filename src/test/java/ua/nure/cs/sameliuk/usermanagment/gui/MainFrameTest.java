102c50af8d24ccc6dc57d75ddea92b05c0d7de24
AndriiSameliuk <andrii.sameliuk@teamdev.com>
2019-12-07 15:25:07 +0200

    Change `testAddUser` in `MainFrameTest`

--- src/test/java/ua/nure/cs/sameliuk/usermanagment/gui/MainFrameTest.java
+++ src/test/java/ua/nure/cs/sameliuk/usermanagment/gui/MainFrameTest.java
@@ -9,6 +9,7 @@ import junit.extensions.jfcunit.eventdata.StringEventData;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
 import ua.nure.cs.sameliuk.usermanagment.db.DaoFactory;
 import ua.nure.cs.sameliuk.usermanagment.db.MockDaoFactory;
+import ua.nure.cs.sameliuk.usermanagment.domain.User;
 import ua.nure.cs.sameliuk.usermanagment.util.Message;
 
 import javax.swing.*;
@@ -16,6 +17,7 @@ import java.awt.*;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 public class MainFrameTest extends JFCTestCase {
@@ -42,8 +44,8 @@ public class MainFrameTest extends JFCTestCase {
 
         DaoFactory.init(properties);
         mockUserDao = ((MockDaoFactory) DaoFactory.getInstance()).getMockUserDao();
-
         mockUserDao.expectAndReturn("findAll", new ArrayList());
+
         setHelper(new JFCTestHelper());
         mainFrame = new MainFrame();
         mainFrame.setVisible(true);
@@ -91,6 +93,20 @@ public class MainFrameTest extends JFCTestCase {
     }
 
     public void testAddUser() {
+        String firstName = "John";
+        String lastName = "Doe";
+        Date now = new Date();
+
+        User user = new User(firstName, lastName, now);
+
+        User expectedUser = new User(1L, firstName, lastName ,now);
+        mockUserDao.expectAndReturn("create", user, expectedUser);
+
+        List<User> users = new ArrayList<>();
+        users.add(expectedUser);
+        mockUserDao.expectAndReturn("findAll", users);
+
+
         JTable table = (JTable) find(JTable.class, USER_TABLE_COMPONENT_NAME);
         assertEquals(0, table.getRowCount());
 
@@ -105,11 +121,12 @@ public class MainFrameTest extends JFCTestCase {
         JButton okButton = (JButton) find(JButton.class, "okButton");
         find(JButton.class, "cancelButton");
 
-        getHelper().sendString(new StringEventData(this, firstNameField, "John"));
-        getHelper().sendString(new StringEventData(this, lastNameField, "Doe"));
+
+        getHelper().sendString(new StringEventData(this, firstNameField, firstName));
+        getHelper().sendString(new StringEventData(this, lastNameField, lastName));
 
         DateFormat formatter = DateFormat.getDateInstance();
-        String date = formatter.format(new Date());
+        String date = formatter.format(now);
         getHelper().sendString(new StringEventData(this, dateOfBirthField, date));
 
         getHelper().enterClickAndLeave(new MouseEventData(this, okButton));
