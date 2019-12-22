7fda791099a14965e14003e9bc153f3fd343da3c
AndriiSameliuk <andrii.sameliuk@teamdev.com>
2019-12-22 14:53:12 +0200

    Add `SearchException`

---
+++ src/main/java/ua/nure/cs/sameliuk/usermanagment/agent/SearchException.java
@@ -0,0 +1,9 @@
+package ua.nure.cs.sameliuk.usermanagment.agent;
+
+public class SearchException extends Exception {
+    private String name;
+
+    public SearchException(Exception e) {
+
+    }
+}
